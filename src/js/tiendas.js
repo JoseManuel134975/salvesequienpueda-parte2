2bd0a6f88059de6b4a5b4d05cb12f833b7215a43
José Manuel <josemanuel3606@gmail.com>
2024-11-02 16:56:54 +0100

    Tiendas.js cambiado

--- src/js/tiendas.js
+++ src/js/tiendas.js
@@ -1,26 +1,20 @@
 /**
- * @description Hace uso de la API de LeafletJS para crear mapas interactivos de las diferentes tiendas
+ * @description Inicializa un mapa de Leaflet en una ubicación específica y añade un marcador con un popup.
+ * @param {string} idMapa - El ID del contenedor del mapa.
+ * @param {Array<number>} coordenadas - Las coordenadas [lat, lng] para centrar el mapa.
+ * @param {string} mensajePopup - El texto que aparecerá en el popup del marcador.
  */
-// Mapa 1
-var map1 = L.map('map1').setView([37.21311017548976, -3.6149235141018696], 12); // Mercadona
+function inicializarMapa(idMapa, coordenadas, mensajePopup) {
+    var map = L.map(idMapa).setView(coordenadas, 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map1);
-L.marker([37.21311017548976, -3.6149235141018696]).addTo(map1).bindPopup('Mapa 1: Mercadona');
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-// Mapa 2
-var map2 = L.map('map2').setView([37.17505247806386, -3.5946676697269724], 12); // JD
+    L.marker(coordenadas).addTo(map).bindPopup(mensajePopup);
+}
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map2);
-L.marker([37.17505247806386, -3.5946676697269724]).addTo(map2).bindPopup('Mapa 2: JD');
-
-// Mapa 3
-var map3 = L.map('map3').setView([37.21619736082923, -3.6087439584563494], 12); // Burguer King
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map3);
-L.marker([37.21619736082923, -3.6087439584563494]).addTo(map3).bindPopup('Mapa 3: Burguer King');
+// Uso de la función para inicializar los mapas
+inicializarMapa('map1', [37.21311017548976, -3.6149235141018696], 'Mapa 1: Mercadona');
+inicializarMapa('map2', [37.17505247806386, -3.5946676697269724], 'Mapa 2: JD');
+inicializarMapa('map3', [37.21619736082923, -3.6087439584563494], 'Mapa 3: Burguer King');
