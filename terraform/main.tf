bef993f71ac7fbbd88caded8bf99db6e438b6f90
José Manuel <josemanuel3606@gmail.com>
2024-11-03 00:05:30 +0100

    Toques finales

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
+# Proveedor
 provider "aws" {
-  region = "us-east-1" # Cambia a tu región preferida
+  region = "us-east-1" # Región
 }
 
+# Grupo de seguridad
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP traffic"
@@ -22,51 +24,59 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
+# VPC
 resource "aws_vpc" "mi_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
 }
 
+# Subred pública
 resource "aws_subnet" "mi_subred_publica" {
-  vpc_id            = aws_vpc.mi_vpc.id
+  vpc_id            = aws_vpc.mi_vpc.id # Asocia la subred a la VPC
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a" # Cambia según tu región
-  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a" # Zona de disponibilidad
+  map_public_ip_on_launch = true # Se asigna una IP pública automáticamente a las instancias que se creen en esta subred
 }
 
+# Puerta de enlace (router) necesario para salir a internet
 resource "aws_internet_gateway" "mi_igw" {
-  vpc_id = aws_vpc.mi_vpc.id
+  vpc_id = aws_vpc.mi_vpc.id # Asocia a la VPC
 }
 
+# Tabla de enrutamiento
 resource "aws_route_table" "mi_ruta_publica" {
   vpc_id = aws_vpc.mi_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.mi_igw.id
+    cidr_block = "0.0.0.0/0" # Permite tráfico a cualquier sitio
+    gateway_id = aws_internet_gateway.mi_igw.id # Asocia a la puerta de enlace
   }
 }
 
+# Más asociación 
 resource "aws_route_table_association" "mi_asociacion_ruta" {
-  subnet_id      = aws_subnet.mi_subred_publica.id
-  route_table_id = aws_route_table.mi_ruta_publica.id
+  subnet_id      = aws_subnet.mi_subred_publica.id # Subred especificada
+  route_table_id = aws_route_table.mi_ruta_publica.id # Tabla de rutas
 }
 
+# Instancia (lo bueno)
 resource "aws_instance" "mi_servidor_web" {
-  ami           = "ami-06b21ccaeff8cd686" # Reemplaza con el ID de AMI que prefieras
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.mi_subred_publica.id
+  ami           = "ami-06b21ccaeff8cd686" # ID de la imagen para instalar el sistema operativo (se consigue desde las ami asociadas a nuestra región en aws)
+  instance_type = "t2.micro" # Tipo de instancia (esta es el nivel gratuito...)
+  subnet_id     = aws_subnet.mi_subred_publica.id # Subred donde se despliega
 
   vpc_security_group_ids = [aws_security_group.allow_http.id]  # Asocia el grupo de seguridad
 
-  user_data = file("user_data.sh")
+  user_data = file("user_data.sh") # Script ejecutable al iniciar la instancia
 
+  # Etiqueta para nombrar la instancia
   tags = {
     Name = "ServidorWeb"
   }
 }
 
+# Muestra la IP pública de la instancia 
 output "public_ip" {
   value = aws_instance.mi_servidor_web.public_ip
 }
--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# Actualiza y instala Nginx
+# Actualiza el sistema e instala paquetes necesarios (nginx y git)
 sudo yum update -y
 sudo yum install -y nginx
 sudo yum install -y git
+# Inicia el servidor web
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-# Crea el directorio /var/www/html si no existe
+# Crea el directorio donde se alojará la web si no existe
 sudo mkdir -p /usr/share/nginx/html
 
 # Clona tu repositorio en un directorio temporal
-# Cambia <tu-repo-url> por la URL de tu repositorio
 sudo git clone https://github.com/JoseManuel134975/salvesequienpueda-parte2.git /tmp/mi_proyecto
 
-# Copia los archivos de src a /var/www/html
+# Copia los archivos de src a /usr/share/nginx/html
 cp -r /tmp/mi_proyecto/src/* /usr/share/nginx/html
 
 # Limpia el directorio temporal
 rm -rf /tmp/mi_proyecto
 
+# Y cambia los permisos por si acaso hay problemas
 sudo chown -R nginx:nginx /usr/share/nginx/html
 sudo chmod -R 755 /usr/share/nginx/html
