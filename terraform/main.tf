1b7e838a5cae61675a3d93e28b564dbbe7fe8a2b
José Manuel <josemanuel3606@gmail.com>
2024-11-02 21:14:18 +0100

    Prueba Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_vpc" "mi_vpc" {
 resource "aws_subnet" "mi_subred_publica" {
   vpc_id            = aws_vpc.mi_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1" # Cambia según tu región
+  availability_zone = "us-east-1a" # Cambia según tu región
   map_public_ip_on_launch = true
 }
 
