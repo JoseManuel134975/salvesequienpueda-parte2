050d7ba1aa0f4442ac50ae1db40e4d5df3ec4b62
José Manuel <josemanuel3606@gmail.com>
2024-11-09 15:36:33 +0100

    Prueba en ami de Debian + módulo de PHP e index

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 # Actualiza el sistema e instala paquetes necesarios
-sudo yum update -y
-sudo yum install -y nginx
-sudo yum install -y git
+sudo apt update
+sudo apt install apache2
+sudo apt install php libapache2-mod-php
+sudo apt install git
 
 # Inicia el servidor web
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
 # Crea el directorio donde se alojará la web si no existe
-sudo mkdir -p /usr/share/nginx/html
+sudo mkdir -p /var/www/html
 
 # Clona tu repositorio en un directorio temporal
 sudo git clone https://github.com/JoseManuel134975/salvesequienpueda-parte2.git /tmp/mi_proyecto
 
+# Crear el archivo PHP
+sudo echo "<?php phpinfo(); ?>" > /tmp/mi_proyecto/src/index.php
+
 # Copia los archivos de src a /usr/share/nginx/html
-cp -r /tmp/mi_proyecto/src/* /usr/share/nginx/html
+sudo cp -r /tmp/mi_proyecto/src/* /var/www/html
 
 # Limpia el directorio temporal
-rm -rf /tmp/mi_proyecto
+sudo rm -rf /tmp/mi_proyecto
 
 # Y cambia los permisos por si acaso hay problemas
-sudo chown -R nginx:nginx /usr/share/nginx/html
-sudo chmod -R 755 /usr/share/nginx/html
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
